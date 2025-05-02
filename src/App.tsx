8d5d8247498160643f06dafd202b941922dd4dd0
Hyurielh <animehyurielh@gmail.com>
2025-05-02 09:52:27 -0600

    ajustando

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           className="w-32 h-32 md:w-40 md:h-40 rounded-full shadow-2xl border-4 border-orange-400 bg-black"
         />
         <h1 className="text-4xl md:text-5xl font-extrabold text-white mt-4 tracking-widest drop-shadow-lg">
-          <span className="text-blue-400">ANIMA</span>
+          <span className="text-blue-400">ANIM</span>
           <span className="text-orange-400">A</span>
           <span className="text-blue-400">S</span>
         </h1>
